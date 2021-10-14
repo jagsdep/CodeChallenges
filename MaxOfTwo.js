cd1785d4f08976266b7a36282403140aa0f34a7c
Jagruti <jagruti.depan@gmail.com>
2021-10-14 17:57:22 -0500

    File Added

---
+++ MaxOfTwo.js
@@ -0,0 +1,14 @@
+//Max of Two
+//x = 1 
+//y = 2
+//Write a block of code that print the largest value
+//Print out "the values are identical" if x & y are same
+let x = 1;
+let y = 4;
+if(x<y){
+  console.log(y)
+}
+else if ( x == y){
+  console.log("The values are identical.")
+}
+else{console.log(x)} 
