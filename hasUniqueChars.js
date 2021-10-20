f9d66bb8af6e65717e6a91d4118d12ff1b318902
Jagruti <jagruti.depan@gmail.com>
2021-10-20 09:55:32 -0500

    Code entered

---
+++ hasUniqueChars.js
@@ -0,0 +1,18 @@
+//code Challenge with Icarus Romero
+
+
+function hasUniqueChar(str){
+    for(let i = 0; i < str.length; i++){
+
+    
+        for(let j = i + 1; j < str.length; j++){
+
+        
+            if (str[i] == str[j])
+                return false;
+    }
+
+     } 
+    return true;
+}
+ console.log(hasUniqueChar("Born"))
