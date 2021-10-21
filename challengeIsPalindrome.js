5167ee9b09d1352155c7fcff82a112a309a4ab36
Jagruti <jagruti.depan@gmail.com>
2021-10-21 11:06:40 -0500

    Coded

---
+++ challengeIsPalindrome.js
@@ -0,0 +1,5 @@
+// coded with Cody Dupis. Write your code below
+const isPalindrome = (String) =>{
+    return String  === String.split("").reverse("").join("")
+} 
+console.log(isPalindrome("racecar"))
